8148aaa44fc3f89d3c5d98a78eeeab5f88982936
Kyle Payne <kpayne@mobilsense.com>
2019-09-13 16:34:04 -0600

    added logic to solve larger puzzles and non-determinable puzzles (it will recursively guess )

--- 3kyu/Sudoku_Solver.js
+++ 3kyu/Sudoku_Solver.js
@@ -1,3 +1,4 @@
+
 /*
 Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
 
@@ -27,15 +28,32 @@ sudoku(puzzle);
 [9,6,1,5,3,7,2,8,4],
 [2,8,7,4,1,9,6,3,5],
 [3,4,5,2,8,6,1,7,9]] 
+
+
+puzzle = [
+   [2,0,0,1,0,4,0,0,6],
+   [0,0,8,5,0,3,0,0,0],
+   [0,0,0,8,0,0,0,0,0],
+   [0,0,0,2,9,0,7,0,1],
+   [7,0,0,0,0,0,0,8,0],
+   [3,0,2,0,0,0,5,0,0],
+   [8,6,0,0,0,0,0,1,9],
+   [0,2,9,0,0,0,0,0,8],
+   [4,0,0,0,0,0,0,0,0]
+]
 */
 
 function sudoku(puzzle) {
-   //return the solved puzzle as a 2d array of 9 x 9 
+   var squareSize = Math.sqrt(puzzle.length);
+
    function allPossibilities(rowNumber, columnNumber) { // returns list of all number possibilities for a given position 
       let row = puzzle[rowNumber];
       let column = getColumn(columnNumber);
       let area = getArea(rowNumber, columnNumber);
-      let possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      let possibilities = [];
+      for (let x = 1; x <= puzzle.length; x++) {
+         possibilities.push(x);
+      }
 
       function removePossibility(found) {
          if (possibilities.indexOf(found) !== -1) {
@@ -45,6 +63,9 @@ function sudoku(puzzle) {
       row.forEach(removePossibility);
       column.forEach(removePossibility);
       area.forEach(removePossibility);
+      if (possibilities.length === 0) {
+         console.log('no possibilities found');
+      }
       return possibilities;
    }
 
@@ -58,10 +79,10 @@ function sudoku(puzzle) {
 
    function getArea(rowNumber, columnNumber) { // returns list of all numbers in a containing quadrant (area) of a position passed in
       let area = [];
-      let rowStart = Math.floor(rowNumber / 3) * 3;
-      let columnStart = Math.floor(columnNumber / 3) * 3;
-      for (let i = rowStart; i < rowStart + 3; i++) {
-         for (let x = columnStart; x < columnStart + 3; x++) {
+      let rowStart = Math.floor(rowNumber / squareSize) * squareSize;
+      let columnStart = Math.floor(columnNumber / squareSize) * squareSize;
+      for (let i = rowStart; i < rowStart + squareSize; i++) {
+         for (let x = columnStart; x < columnStart + squareSize; x++) {
             area.push(puzzle[i][x]);
          }
       }
@@ -69,40 +90,43 @@ function sudoku(puzzle) {
    }
 
    function getAreaNumber(rowNumber, columnNumber) { // returns the area number of the passed in position
-      let num1 = Math.floor(rowNumber / 3);
-      let num2 = Math.floor(columnNumber / 3);
-      let areaNum = (num1 * 3) + num2;
+      let num1 = Math.floor(rowNumber / squareSize);
+      let num2 = Math.floor(columnNumber / squareSize);
+      let areaNum = (num1 * squareSize) + num2;
       return areaNum;
    }
 
    function removeFromRow(rowNumber, value) { // removes value from list of possibilities for each location in a given rowNumber
-      for (let key in zeroMap) {
+      for (let keyString in zeroMap) {
+         let key = keyString.split('-');
          if (key[0] == rowNumber) {
-            let possibilities = zeroMap[key];
+            let possibilities = zeroMap[keyString];
             if (possibilities.includes(value)) {
-               zeroMap[key].splice(possibilities.indexOf(value), 1);
+               zeroMap[keyString].splice(possibilities.indexOf(value), 1);
             }
          }
       }
    }
 
    function removeFromColumn(columnNumber, value) { // removes value from list of possibilities for each location in a given columnNumber
-      for (let key in zeroMap) {
+      for (let keyString in zeroMap) {
+         let key = keyString.split('-');
          if (key[1] == columnNumber) {
-            let possibilities = zeroMap[key];
+            let possibilities = zeroMap[keyString];
             if (possibilities.includes(value)) {
-               zeroMap[key].splice(possibilities.indexOf(value), 1);
+               zeroMap[keyString].splice(possibilities.indexOf(value), 1);
             }
          }
       }
    }
 
    function removeFromArea(areaNumber, value) { // removes value from list of possibilities for each location in a given areaNumber
-      for (let key in zeroMap) {
+      for (let keyString in zeroMap) {
+         let key = keyString.split('-');
          if (key[2] == areaNumber) {
-            let possibilities = zeroMap[key];
+            let possibilities = zeroMap[keyString];
             if (possibilities.includes(value)) {
-               zeroMap[key].splice(possibilities.indexOf(value), 1);
+               zeroMap[keyString].splice(possibilities.indexOf(value), 1);
             }
          }
       }
@@ -118,7 +142,7 @@ function sudoku(puzzle) {
             let row = i;
             let column = j;
             let area = getAreaNumber(row, column);
-            let key = '' + row + column + area;
+            let key = row + '-' + column + '-' + area;
             let possibilities = allPossibilities(row, column);
 
             zeroMap[key] = possibilities;
@@ -128,21 +152,57 @@ function sudoku(puzzle) {
 
    // while there are still zeros, iterate over map and fill in puzzle (replace zeros where possibilities === 1, then remove that number from other possibilities lists)
    while (Object.keys(zeroMap).length > 0) {
-      for (let key in zeroMap) {
-         if (zeroMap[key].length === 1) { // if only one possibility exists
-            let value = zeroMap[key][0]; // determine the value
+      let nothingFound = true;
+      for (let keyString in zeroMap) {
+         let key = keyString.split('-');
+         if (zeroMap[keyString].length === 1) { // if only one possibility exists
+            let value = zeroMap[keyString][0]; // determine the value
             let row = parseInt(key[0]);
             let column = parseInt(key[1]);
             let area = parseInt(key[2]);
             puzzle[row][column] = value; // replace zero in puzzle with value
 
-            delete zeroMap[key]; // delete the key
+            delete zeroMap[keyString]; // delete the key
 
             // remove value from other keys' possibilities
             removeFromRow(row, value);
             removeFromColumn(column, value);
             removeFromArea(area, value)
+
+            nothingFound = false;
+         } else if (zeroMap[keyString].length === 0) {
+            return false;
          }
+      }
+
+      if (nothingFound) {
+         //nothing was done on this iteration of the loop so start guessing
+         console.log("Nothing found. Starting to guess.");
+         let minPossibilities = Infinity;
+         let guessKey;
+         for (let key in zeroMap) { // finding the key with the least amount of possibilities
+            if (zeroMap[key].length < minPossibilities) {
+               minPossibilities = zeroMap[key].length;
+               guessKey = key;
+            }
+         }
+         let retVal = false;
+         zeroMap[guessKey].forEach((value) => { // recursively guessing each position
+            if (retVal) return;
+            let key = guessKey.split('-');
+            let row = parseInt(key[0]);
+            let column = parseInt(key[1]);
+            let newPuzzle = [];
+            puzzle.forEach((row) => { // creating a copy of the puzzle
+               newPuzzle.push(row.slice(0));
+            });
+            newPuzzle[row][column] = value;
+            let answer = sudoku(newPuzzle);
+            if (answer) {
+               retVal = answer;
+            }
+         });
+         return retVal;
       }
    }
 
@@ -150,18 +210,36 @@ function sudoku(puzzle) {
    return puzzle;
 }
 
+// let puzzle = [
+//    [2, 0, 0, 1, 0, 4, 0, 0, 6],
+//    [0, 0, 8, 5, 0, 3, 0, 0, 0],
+//    [0, 0, 0, 8, 0, 0, 0, 0, 0],
+//    [0, 0, 0, 2, 9, 0, 7, 0, 1],
+//    [7, 0, 0, 0, 0, 0, 0, 8, 0],
+//    [3, 0, 2, 0, 0, 0, 5, 0, 0],
+//    [8, 6, 0, 0, 0, 0, 0, 1, 9],
+//    [0, 2, 9, 0, 0, 0, 0, 0, 8],
+//    [4, 0, 0, 0, 0, 0, 0, 0, 0]
+// ];
 
-
-var puzzle = [
-   [5, 3, 0, 0, 7, 0, 0, 0, 0],
-   [6, 0, 0, 1, 9, 5, 0, 0, 0],
-   [0, 9, 8, 0, 0, 0, 0, 6, 0],
-   [8, 0, 0, 0, 6, 0, 0, 0, 3],
-   [4, 0, 0, 8, 0, 3, 0, 0, 1],
-   [7, 0, 0, 0, 2, 0, 0, 0, 6],
-   [0, 6, 0, 0, 0, 0, 2, 8, 0],
-   [0, 0, 0, 4, 1, 9, 0, 0, 5],
-   [0, 0, 0, 0, 8, 0, 0, 7, 9]
-];
+let puzzle = 
+[
+   [0,11,9,0,0,16,13,4,0,0,14,0,10,6,15,0],
+   [4,12,15,0,3,6,0,11,0,5,0,1,16,7,14,2],
+   [1,0,6,0,15,2,0,0,11,9,10,0,0,0,8,0],
+   [0,13,0,0,0,1,0,0,4,6,0,15,0,0,0,0],
+   [0,0,0,0,0,0,15,0,8,1,5,3,0,4,11,7],
+   [6,0,1,0,0,12,8,0,9,0,0,2,0,0,3,0],
+   [14,0,4,13,6,0,0,3,0,12,7,10,8,0,2,0],
+   [3,8,0,0,4,7,2,0,6,0,0,0,0,12,16,5],
+   [13,0,0,16,0,8,14,10,3,4,15,0,12,5,1,11],
+   [0,0,0,6,2,0,0,1,10,0,11,0,15,3,0,9],
+   [7,0,0,12,0,4,0,15,5,0,9,14,0,0,0,0],
+   [10,0,0,8,0,0,11,0,0,0,1,12,4,0,13,16],
+   [0,0,0,0,0,0,7,0,15,2,0,0,0,0,12,3],
+   [0,0,7,0,0,10,6,0,1,8,0,13,11,0,9,14],
+   [8,6,5,0,0,3,0,0,14,0,0,9,0,0,0,0],
+   [0,16,0,2,0,0,0,14,0,10,0,0,0,0,0,0]
+]
 
 console.log(sudoku(puzzle));
