129266bc6051d2d32c5220860ab68e6aab774563
Kyle Payne <kpayne@mobilsense.com>
2019-09-18 11:35:26 -0600

    completed

---
+++ 5kyu/Count_9s_from_1_to_n.js
@@ -0,0 +1,33 @@
+/*
+It's 9 time!
+
+I want to count from 1 to n. How many times will I use a '9'?
+
+9, 19, 91.. will contribute one '9' each, 99, 199, 919.. wil contribute two '9's each...etc
+
+Note: n will always be a postive integer.
+
+number9(8) //should return 0
+number9(9) //should return 1
+number9(10) //should return 1
+number9(98) //should return 18
+number9(100) //should return 20
+*/
+
+
+
+// return the number of 9's used to count out 1 to n
+function number9(n) {
+   var count = 0;
+   for (var i = 1; i <= n; i *= 10) {
+      var curr = Math.floor(n / i);
+      count += Math.floor(curr / 10) * i;
+      if (curr % 10 === 9) {
+         count += (n % i) + 1;
+      };
+   };
+
+   return count;
+};
+
+number9(100)
