f434c02ca8a5eb2415d1e7f86c5733939e1cd13a
Kyle Payne <kpayne@mobilsense.com>
2019-10-02 13:42:22 -0600

    completed

---
+++ 5kyu/Regex_Password_Validation.js
@@ -0,0 +1,17 @@
+/*
+https://www.codewars.com/kata/regex-password-validation/train/javascript
+
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a number
+Valid passwords will only be alphanumeric characters.
+*/
+
+function validate(password) {
+   return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/.test(password);
+}
+
+validate('Password123')
