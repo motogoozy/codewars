34d0d448687017f9f12c4f1e48c801a9862491c2
Kyle Payne <kpayne@mobilsense.com>
2019-11-15 15:36:59 -0700

    refactored & completed

--- 6kyu/Title_Case.js
+++ 6kyu/Title_Case.js
@@ -1,25 +1,33 @@
-function titleCase(title, minorWords) {
-   var titleCased = [];
-   var words = title.toLowerCase().split(" ");
-   if (minorWords) {
-      var minorWordsArr = minorWords.toLowerCase().split(" ");
-   }
+/*
+https://www.codewars.com/kata/title-case/train/javascript
 
-   for (var num = 0; num < words.length; num++) {
-      var currentWord = words[num];
-      if (minorWords == undefined) {
-         titleCased.push(currentWord[0].toUpperCase() + currentWord.slice(1));
-      } else {
-         if (!minorWordsArr.includes(currentWord) || num === 0) {
-            titleCased.push(currentWord[0].toUpperCase() + currentWord.slice(1));
-         } else {
-            titleCased.push(currentWord);
-         }
-      }
-   }
-   return titleCased.join(" ");
+A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+###Arguments (Haskell)
+
+First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+Second argument: the original string to be converted.
+###Arguments (Other languages)
+
+First argument (required): the original string to be converted.
+Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+###Example
+
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+*/
+
+const titleCase = (title, minorWords) => {
+	if (!title) return title;
+
+	let cap = word => word[0].toUpperCase() + word.slice(1);
+	let minors = (minorWords || '').toLowerCase().split(' ');
+	let result = title.toLowerCase().replace(/\S+/g, w => minors.indexOf(w) === -1 ? cap(w) : w);
+
+	return cap(result);
 }
 
-console.log(titleCase('a clash of KINGS', 'a an the of'));
-console.log(titleCase('the quick brown fox'));
-titleCase('THE WIND IN THE WILLOWS', 'The In');
+titleCase('a clash of KINGS', 'a an THE of');
