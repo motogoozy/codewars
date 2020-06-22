daf4e4ae1d32f7f333d142ff19d830ce83c12098
Kyle Payne <kspayne93@gmail.com>
2020-06-22 09:55:39 -0600

    updated

--- 3kyu/Sudoku_Solver.js
+++ 3kyu/Sudoku_Solver.js
@@ -1,8 +1,6 @@
 
 /*
-Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
-
-The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+Write a function that will solve a Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
 
 For Sudoku rules, see the Wikipedia article.
 
