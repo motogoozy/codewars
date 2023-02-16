1861b949c75fd365db70d2f23a7f558222ab437b
Kyle Payne <43189095+motogoozy@users.noreply.github.com>
2023-02-16 15:11:38 -0700

    fix

--- 5kyu/Human_Readable_Time.js
+++ 5kyu/Human_Readable_Time.js
@@ -1,24 +1,36 @@
 /* 
-Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid. 
+https://www.codewars.com/kata/52685f7382004e774f0001f7/javascript
 
-"()"              =>  true
-")(()))"          =>  false
-"("               =>  false
-"(())((()())())"  =>  true
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
 */
 
-function validParentheses(parens) {
-   let openCount = 0;
-   let closeCount = 0;
-   if (parens.startsWith('(') && parens.endsWith(')')) {
-      parens.split('').forEach(p => {
-         if (p === '(') openCount++;
-         else closeCount++
-      });
-      if (openCount === 0 && closeCount === 0) return false;
-      else if (openCount === closeCount) return true;
-      else return false;
-   } else return false;
-}
+function humanReadable(seconds) {
+  let remainingSeconds = seconds % 60;
+  let minutes = Math.floor(seconds / 60);
+  let remainingMinutes = minutes % 60;
+  let hours = Math.floor(minutes / 60);
 
-validParentheses("(())((()())())")
+  if (hours.toString().split('').length < 2) {
+    hours = `0${hours}`
+  };
+  if (remainingSeconds.toString().split('').length < 2) {
+    remainingSeconds = `0${remainingSeconds}`
+  };
+  if (remainingMinutes.toString().split('').length < 2) {
+    remainingMinutes = `0${remainingMinutes}`
+  };
+
+  return `${hours}:${remainingMinutes}:${remainingSeconds}`
+};
+
+console.log(humanReadable(45296)); // '12:34:56'
+console.log(humanReadable(86399)); // '23:59:59'
+console.log(humanReadable(86400)); // '24:00:00'
+console.log(humanReadable(359999)); // '99:59:59'
