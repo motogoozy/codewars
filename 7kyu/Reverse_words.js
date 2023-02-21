6ff1192ec29e13305119da15b480df87532476b4
Kyle Payne <43189095+motogoozy@users.noreply.github.com>
2023-02-21 12:32:07 -0700

    fix

--- 7kyu/Reverse_words.js
+++ 7kyu/Reverse_words.js
@@ -1,13 +1,13 @@
 /*
 https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
 
-Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Create a function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
 Examples
 "This is an example!" ==> "sihT si na !elpmaxe"
 "double  spaces"      ==> "elbuod  secaps"
 */
 
-reverseWords = (str) => str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+const reverseWords = (str) => str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
 
 reverseWords('The quick brown fox jumps over the lazy dog.');
